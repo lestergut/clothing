1110a586f4e5191276d9c654aecf45ffc62b587c
Lester <lestergut@gmail.com>
2019-12-31 08:10:42 -0500

    Experimenting with react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 
+import { Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage';
+
+const Topic = props => {
+   console.log(props);
+   return (
+      <div>
+         <h1>TOPIC PAGE</h1>
+      </div>
+   );
+};
+
+const TopicDetail = props => {
+   console.log(props);
+   return (
+      <div>
+         <h1>Topic Detail!</h1>
+      </div>
+   );
+};
 
 function App() {
    return (
       <div>
-         <HomePage />
+         <Route path="/" component={HomePage} />
+         <Route path="/topic" component={Topic} />
+         <Route path="/topic/:id" component={TopicDetail} />
       </div>
    );
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,7 +4,8 @@ import Directory from '../../components/directory/directory';
 
 import './homepage.scss';
 
-const HomePage = () => {
+const HomePage = props => {
+   console.log(props);
    return (
       <div className="homepage">
          <Directory />
