9112406ca50d6bebd9d58d2cdbc5497bff97c6d5
Lester <lestergut@gmail.com>
2020-01-05 14:07:35 -0500

    form-input, custom-button components added and sigin form without the backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
          <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignInAndSignUp} />
          </Switch>
       </div>
    );
